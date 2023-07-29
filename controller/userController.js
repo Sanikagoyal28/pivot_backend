caf7601c99d515f8d7349dc39c2f47cb03590a2a
sanika <sanika2110118@akgec.ac.in>
2023-07-29 16:59:36 +0530

    dashboard api added

--- controller/userController.js
+++ controller/userController.js
@@ -48,11 +48,12 @@ const get_questions = async (req, res, next) => {
 
 const submit_quiz = async (req, res, next) => {
     try {
-
         const { username, category, difficulty, correct_ans, total_ques, score } = req.body
 
-        if (!username || !category || !difficulty || !correct_ans || !total_ques || !score)
+        if (!username || !category || !difficulty || !total_ques) {
+            console.log("yes")
             return next(new Errorhandler("All inputs are required", 400))
+        }
 
         const user = await User.create({
             username: username,
@@ -71,8 +72,57 @@ const submit_quiz = async (req, res, next) => {
     }
 }
 
+const get_results = async (req, res, next) => {
+    try {
+
+        const { category } = req.params
+
+        if (!category)
+            return next(new Errorhandler("Category is required", 400))
+
+        var results = []
+        // if (difficulty) {
+        //     results = await User.find({
+        //         category: category, difficulty: difficulty.toLowerCase()
+        //     }).sort({ score: -1 }).populate("category")
+        // }
+        // else {
+        results = await User.find({
+            category: category
+        }).sort({ score: -1 }).populate("category")
+        // }
+
+        // console.log(results)
+        return res.status(201).json({ success: true, msg: "Results", results })
+    }
+    catch (err) {
+        return next(err)
+    }
+}
+
+const sort_result = async (req, res, next) => {
+    try {
+
+        const { category, difficulty } = req.params
+
+        if (!category)
+            return next(new Errorhandler("Category is required", 400))
+
+        const results = await User.find({
+            category: category, difficulty: difficulty.toLowerCase()
+        }).sort({ score: -1 }).populate("category")
+
+        return res.status(201).json({ success: true, msg: "Results", results })
+    }
+    catch (err) {
+        return next(err)
+    }
+}
+
 module.exports = {
     num_of_question,
     get_questions,
-    submit_quiz
+    submit_quiz,
+    get_results,
+    sort_result
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,7 @@ const UserController = require("../controller/userController")
 userRoutes.get("/num_of_ques/:category/:difficulty", UserController.num_of_question)
 userRoutes.get("/questions/:category/:difficulty/:num_of_ques", UserController.get_questions)
 userRoutes.post("/submit", UserController.submit_quiz)
+userRoutes.get("/dashboard/:category", UserController.get_results)
+userRoutes.get("/dashboard/:category/:difficulty", UserController.sort_result)
 
 module.exports = { userRoutes }
